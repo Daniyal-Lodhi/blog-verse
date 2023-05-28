24d11198fbf19ebb14308b5a76a3ea2ae2d64f78
Daniyal-Lodhi <98033919+Daniyal-Lodhi@users.noreply.github.com>
2023-05-29 01:57:40 +0500

    first

---
+++ src/Components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './css/Navbar.css'
+
+const Home = () => {
+  return (
+    <div className="" id='header'>
+        <div className='d-flex flex-column' id='homeflex'>
+        <div><h1> <span className='text-warning'>Find </span> a vast range</h1></div>
+        <div><h1>of blogs on</h1></div>
+        <div className='text-primary'><h1>BlogVerse</h1></div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Home
